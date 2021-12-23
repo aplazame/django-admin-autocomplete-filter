779c31cde57a49e55903adb0a12f53c6f8c73a8c
Antonio <antonio.irizar@aplazame.com>
2021-12-23 12:42:10 +0100

    Antonioirizar/fix autocomplete json view (#3)
    * remove get method of library to use by default of django

--- admin_auto_filters/views.py
+++ admin_auto_filters/views.py
@@ -1,31 +1,11 @@
 from django.apps import apps
 from django.contrib.admin.views.autocomplete import AutocompleteJsonView as Base
 from django.core.exceptions import FieldDoesNotExist, PermissionDenied
-from django.http import Http404, JsonResponse
+from django.http import Http404
 
 
 class AutocompleteJsonView(Base):
     """Overriding django admin's AutocompleteJsonView"""
-
-    @staticmethod
-    def display_text(obj):
-        """
-        Hook to specify means for converting object to string for endpoint.
-        """
-        return str(obj)
-
-    def get(self, request, *args, **kwargs):
-        self.term = request.GET.get('term', '')
-        self.paginator_class = self.model_admin.paginator
-        self.object_list = self.get_queryset()
-        context = self.get_context_data()
-        return JsonResponse({
-            'results': [
-                {'id': str(obj.pk), 'text': self.display_text(obj)}
-                for obj in context['object_list']
-            ],
-            'pagination': {'more': context['page_obj'].has_next()},
-        })
 
     def process_request(self, request):
         """
