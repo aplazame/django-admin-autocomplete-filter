1f0e4c19076fce4d6e529589b21f129c35d3c018
antonioIrizar <antonioirizar@gmail.com>
2021-12-23 11:56:22 +0100

    remove get method of libary to use by defualt of django

--- admin_auto_filters/views.py
+++ admin_auto_filters/views.py
@@ -7,26 +7,6 @@ from django.http import Http404, JsonResponse
 class AutocompleteJsonView(Base):
     """Overriding django admin's AutocompleteJsonView"""
 
-    @staticmethod
-    def display_text(obj):
-        """
-        Hook to specify means for converting object to string for endpoint.
-        """
-        return str(obj)
-
-    def get(self, request, *args, **kwargs):
-        self.term = request.GET.get('term', '')
-        self.paginator_class = self.model_admin.paginator
-        self.object_list = self.get_queryset()
-        context = self.get_context_data()
-        return JsonResponse({
-            'results': [
-                {'id': str(obj.pk), 'text': self.display_text(obj)}
-                for obj in context['object_list']
-            ],
-            'pagination': {'more': context['page_obj'].has_next()},
-        })
-
     def process_request(self, request):
         """
         Validate request integrity, extract and return request parameters.
